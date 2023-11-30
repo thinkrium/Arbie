408ac2e796c7d95ef5bd378049ede1005ef62bb2
Thom Simmons <thomedy@thinkrium.com>
2023-11-30 12:35:52 -0600

    moved functionality to brain

--- Brain/Functionality/Functionality.cpp
+++ Brain/Functionality/Functionality.cpp
@@ -8,33 +8,38 @@ namespace Brain {
     namespace Functionality {
         void Functionality::Run() {
 
-            Servo_Motor_Profile profile;
+            try {
+                Servo_Motor_Profile profile;
 
-            Servo_Motor
-                    pitchServoMotor(0,0, profile ),
-                    yawServoMotor(1, 0,profile ),
-                    rollServoMotor(2, 0,profile ) ;
+                Servo_Motor
+                        pitchServoMotor(0, 0, profile),
+                        yawServoMotor(1, 0, profile),
+                        rollServoMotor(2, 0, profile);
 
-            Axis xAxis(pitchServoMotor),
-                    yAxis(yawServoMotor),
-                    zAxis(rollServoMotor);
+                Axis xAxis(pitchServoMotor),
+                        yAxis(yawServoMotor),
+                        zAxis(rollServoMotor);
 
-            map<Relative_Axes, Axis> axes = {
-                    {Relative_Axes::x, xAxis},
-                    {Relative_Axes::y, yAxis  },
-                    {Relative_Axes::z, zAxis }
-            };
+                map<Relative_Axes, Axis> axes = {
+                        {Relative_Axes::x, xAxis},
+                        {Relative_Axes::y, yAxis},
+                        {Relative_Axes::z, zAxis}
+                };
 
-            Shoulder shoulder(
-                    axes
-            );
+                Shoulder shoulder(
+                        axes
+                );
 
-            shoulder.turnOnAxis(Relative_Axes::x, Rotation_Direction::Counter_Clockwise, 30);
-            shoulder.turnOnAxis(Relative_Axes::y, Rotation_Direction::Counter_Clockwise, 30);
-            shoulder.turnOnAxis(Relative_Axes::z, Rotation_Direction::Counter_Clockwise, 30);
+                shoulder.turnOnAxis(Relative_Axes::x, Rotation_Direction::Counter_Clockwise, 30);
+                shoulder.turnOnAxis(Relative_Axes::y, Rotation_Direction::Counter_Clockwise, 30);
+                shoulder.turnOnAxis(Relative_Axes::z, Rotation_Direction::Counter_Clockwise, 30);
 
-            Logger::Success("Main" , "Main", Error_Messaging::No_Error);
+                Logger::Success("Brain", "Run", Error_Messaging::No_Error);
+            }
+            catch (exception e) {
+                Logger::Success("Brain", "Run", Error_Messaging::Generic_Error);
 
+            }
         }
     } // Brain
 } // Functionality
