6ad38240356b82b3e00033e84d895cd5feb5a089
Thom Simmons <thomedy@thinkrium.com>
2024-05-21 17:51:03 -0500

    includes opencv

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "Brain/Activity/Activity.h"
 
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/dnn.hpp"
+
+using namespace cv;
+using namespace cv::dnn;
+
 using namespace Brain::Activity;
 
 int main() {
+
+    Mat frame;
 
     Brain::Activity::Activity::Run();
 
