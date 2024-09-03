871133a70be07bc272480e386590e12a6a2dfb0f
thomedy <thomedy@thinkrium.com>
2024-09-03 16:35:30 -0500

    they are talking to eachother

--- Body/Movement/Motors/Base/BaseMotor.h
+++ Body/Movement/Motors/Base/BaseMotor.h
@@ -12,6 +12,7 @@
 #include <net/if.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <utility>
 #include <sys/ioctl.h>
 
 
@@ -23,20 +24,36 @@ namespace Motors {
 class BaseMotor {
 
 private:
-
-    int s ;
     struct sockaddr_can canSocket;
-    struct ifreq   ifr;
+    struct ifreq ifr;
     struct can_frame canFrame;
 
+    int motorID;
+    int socketID ;
+
 public:
+    int get_motor_id();
+    void set_motor_id(int motor_id);
+    int get_socket_id();
 
+    void set_socket_id(int socket_id);
 
+    BaseMotor(int motor_id);
+
+    struct sockaddr_can get_can_socket();
+
+    void set_can_socket(struct sockaddr_can can_socket);
+
+    struct ifreq get_ifr();
+    void set_ifr(struct ifreq ifr);
+    struct can_frame get_can_frame();
+    void set_can_frame(struct can_frame can_frame);
     BaseMotor() = default;
 
-    int initiateControllerConnection();
-    int sendControllerInstructions(int speedPercentage, int destinationAngle, int rotationalDirection);
-    int closeControllerConnection();
+
+    int initiate_controller_connection();
+    int send_controller_instructions(int speedPercentage, int destinationAngle, int rotationalDirection);
+    int close_controller_connection();
 
     ~BaseMotor() = default;
 };
---
+++ Common/MotorIDs.h
@@ -0,0 +1,10 @@
+//
+// Created by thom on 9/3/24.
+//
+
+#ifndef MOTORIDS_H
+#define MOTORIDS_H
+
+#define testMotor 0x0001
+
+#endif //MOTORIDS_H
