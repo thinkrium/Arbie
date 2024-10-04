a89ae04e2e9759dcbca1186171340b38af085011
thomedy <thomedy@thinkrium.com>
2024-10-04 08:04:18 -0500

    teees

--- Body/main.cpp
+++ Body/main.cpp
@@ -4,15 +4,18 @@
 #include "Movement/Motors/Base/BaseMotor.h"
 #include "Movement/Motors/Stepper/StepperMotor.h"
 #include "NervousSystem/NervousSystem.h"
+#include "../Shared/MovementProfiles.h"
 
 using namespace Arbie::Body;
 using namespace Movement::Motors;
+using namespace Shared::Profiles;
 
 int main() {
-
+    // float a = MovementProfiles::ForwardMovement.move.at( MovementProfiles::MovementStages::stage1)
+    //         .relative_joint_movement.at(BodyPart.LeftKnee);
 
     StepperMotor stepper_motor;
-     stepper_motor.set_motor_id(0x001);
+    stepper_motor.set_motor_id(0x001);
      stepper_motor.initiate_controller_connection();
 //    for (int i = 0 ; i < 5; i++) {
      // std::cout << "switch: " << i << endl;
