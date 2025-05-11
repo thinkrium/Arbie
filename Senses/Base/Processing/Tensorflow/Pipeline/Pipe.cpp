efff9f561446cdcfe587fc3791a655e19bd403c2
thomedy <thomedy@thinkrium.com>
2025-05-11 11:09:31 -0500

    added tf libs

---
+++ Senses/Base/Processing/Tensorflow/Pipeline/Pipe.cpp
@@ -0,0 +1,20 @@
+//
+// Created by thom on 5/10/25.
+//
+
+#include "Pipe.h"
+
+namespace Arbe {
+namespace Senses {
+namespace Sight {
+namespace Base {
+namespace Processing {
+namespace Tensorflow {
+namespace Pipeline {
+} // Pipeline
+} // Tensorflow
+} // Processing
+} // Base
+} // Sight
+} // Senses
+} // Arbe
---
+++ Senses/Base/Processing/Tensorflow/Pipeline/Pipe.h
@@ -0,0 +1,31 @@
+//
+// Created by thom on 5/10/25.
+//
+// #include "tensorflow/lite/interpreter.h"
+// #include "tensorflow/lite/kernels/register.h"
+// #include "tensorflow/lite/model.h"
+
+#ifndef PIPE_H
+#define PIPE_H
+
+namespace Arbe {
+namespace Senses {
+namespace Sight {
+namespace Base {
+namespace Processing {
+namespace Tensorflow {
+namespace Pipeline {
+
+class Pipe {
+
+};
+
+} // Pipeline
+} // Tensorflow
+} // Processing
+} // Base
+} // Sight
+} // Senses
+} // Arbe
+
+#endif //PIPE_H
