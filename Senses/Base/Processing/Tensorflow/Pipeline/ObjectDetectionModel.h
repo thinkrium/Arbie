8ac83e69a7ccfd1ec1b10fc766bb30d1547abd13
thomedy <thomedy@thinkrium.com>
2025-05-14 09:16:15 -0500

    Adding model objects

---
+++ Senses/Base/Processing/Tensorflow/Pipeline/ObjectDetectionModel.h
@@ -0,0 +1,38 @@
+//
+// Created by thom on 5/13/25.
+//
+
+#ifndef OBJECTDETECTIONMODEL_H
+#define OBJECTDETECTIONMODEL_H
+#include "Model.h"
+
+namespace Arbie {
+namespace Senses {
+namespace Sight {
+namespace Base {
+namespace Processing {
+namespace Tensorflow {
+namespace Pipeline {
+
+class ObjectDetectionModel : public Model {
+public:
+ObjectDetectionModel() = default;
+
+    void Preprocess() override;
+
+    void Process() override;
+
+    void Postprocess() override;
+
+~ObjectDetectionModel() override  = default;
+};
+
+} // Pipeline
+} // Tensorflow
+} // Processing
+} // Base
+} // Sight
+} // Senses
+} // Arbie
+
+#endif //OBJECTDETECTIONMODEL_H
